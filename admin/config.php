9843ccc6b66426673286e6a0ba9081e63813cc17
gaodihu <gaodihu@126.com>
2015-12-31 11:44:33 +0800

    Signed-off-by: init

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.dev.com/admin/');
+define('HTTP_CATALOG', 'http://www.dev.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.dev.com/admin/');
+define('HTTPS_CATALOG', 'http://www.dev.com/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/www/opencart/opencart-2.1.0.1/admin/');
+define('DIR_SYSTEM', 'E:/www/opencart/opencart-2.1.0.1/system/');
+define('DIR_LANGUAGE', 'E:/www/opencart/opencart-2.1.0.1/admin/language/');
+define('DIR_TEMPLATE', 'E:/www/opencart/opencart-2.1.0.1/admin/view/template/');
+define('DIR_CONFIG', 'E:/www/opencart/opencart-2.1.0.1/system/config/');
+define('DIR_IMAGE', 'E:/www/opencart/opencart-2.1.0.1/image/');
+define('DIR_CACHE', 'E:/www/opencart/opencart-2.1.0.1/system/storage/cache/');
+define('DIR_DOWNLOAD', 'E:/www/opencart/opencart-2.1.0.1/system/storage/download/');
+define('DIR_LOGS', 'E:/www/opencart/opencart-2.1.0.1/system/storage/logs/');
+define('DIR_MODIFICATION', 'E:/www/opencart/opencart-2.1.0.1/system/storage/modification/');
+define('DIR_UPLOAD', 'E:/www/opencart/opencart-2.1.0.1/system/storage/upload/');
+define('DIR_CATALOG', 'E:/www/opencart/opencart-2.1.0.1/catalog/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'myopencart2');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.dev.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.dev.com/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/www/opencart/opencart-2.1.0.1/catalog/');
+define('DIR_SYSTEM', 'E:/www/opencart/opencart-2.1.0.1/system/');
+define('DIR_LANGUAGE', 'E:/www/opencart/opencart-2.1.0.1/catalog/language/');
+define('DIR_TEMPLATE', 'E:/www/opencart/opencart-2.1.0.1/catalog/view/theme/');
+define('DIR_CONFIG', 'E:/www/opencart/opencart-2.1.0.1/system/config/');
+define('DIR_IMAGE', 'E:/www/opencart/opencart-2.1.0.1/image/');
+define('DIR_CACHE', 'E:/www/opencart/opencart-2.1.0.1/system/storage/cache/');
+define('DIR_DOWNLOAD', 'E:/www/opencart/opencart-2.1.0.1/system/storage/download/');
+define('DIR_LOGS', 'E:/www/opencart/opencart-2.1.0.1/system/storage/logs/');
+define('DIR_MODIFICATION', 'E:/www/opencart/opencart-2.1.0.1/system/storage/modification/');
+define('DIR_UPLOAD', 'E:/www/opencart/opencart-2.1.0.1/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'myopencart2');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
